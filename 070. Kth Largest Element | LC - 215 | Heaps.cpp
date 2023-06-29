ed19a67924591aa6059400545197512f3826a007
Mohammad Amaan Sharif <97755687+amaan-272@users.noreply.github.com>
2023-06-29 05:57:22 +0530

    Create 070. Kth Largest Element | LC - 215 | Heaps.cpp
    Problem Statement
    Coding Ninjas: https://www.codingninjas.com/studio/problems/kth-smallest-and-largest-element-of-array_8230829?challengeSlug=striver-sde-challenge
    LeetCode: https://leetcode.com/problems/kth-largest-element-in-an-array/
    
    Submission (June 28)
    Coding Ninjas: https://www.codingninjas.com/studio/problems/kth-smallest-and-largest-element-of-array_8230829?challengeSlug=striver-sde-challenge&leftPanelTab=1
    LeetCode: https://leetcode.com/submissions/detail/981966628/

---
+++ 070. Kth Largest Element | LC - 215 | Heaps.cpp
@@ -0,0 +1,18 @@
+//CodeStudio Code
+#include <bits/stdc++.h>
+
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k) {
+	sort(begin(arr), end(arr));
+	return {arr[k-1], arr[n-k]};
+}
+
+//LeetCode Code
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(begin(nums), end(nums));
+        k--;
+        while(k--) pq.pop();
+        return pq.top();
+    }
+};
