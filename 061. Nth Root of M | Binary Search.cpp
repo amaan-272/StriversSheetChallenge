dc7748b950692ddec2154d34021e5f1d12ee2761
Mohammad Amaan Sharif <97755687+amaan-272@users.noreply.github.com>
2023-06-29 10:51:48 +0530

    Create 061. Nth Root of M | Binary Search.cpp
    Problem Statement
    Coding Ninjas: https://www.codingninjas.com/studio/problems/find-nth-root-of-m_8230799?challengeSlug=striver-sde-challenge
    
    Submission (June 27)
    Coding Ninjas: https://www.codingninjas.com/studio/problems/find-nth-root-of-m_8230799?challengeSlug=striver-sde-challenge&leftPanelTab=1

---
+++ 061. Nth Root of M | Binary Search.cpp
@@ -0,0 +1,12 @@
+//CodeStudio Code
+int NthRoot(int n, int m) {
+    if(n==1 || m==1) return m;
+    int low {2}, high {m-1};
+    while(low <= high) {
+      int mid {low+(high-low)/2};
+      if(pow(mid, n) == m) return mid;
+      else if(pow(mid, n) > m) high = mid-1;
+      else low = mid+1;
+    }
+    return -1;
+}
