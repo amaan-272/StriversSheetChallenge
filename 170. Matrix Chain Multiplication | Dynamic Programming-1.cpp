5edefcdb18929d9ea2d77233e9d3819962f51a57
Mohammad Amaan Sharif <97755687+amaan-272@users.noreply.github.com>
2023-07-10 21:45:57 +0530

    Create 170. Matrix Chain Multiplication | Dynamic Programming-1.cpp
    Problem Statement
    Coding Ninjas: https://www.codingninjas.com/studio/problems/matrix-chain-multiplication_8230769?challengeSlug=striver-sde-challenge
    
    Submission (July 10-03)
    Coding Ninjas: https://www.codingninjas.com/studio/problems/matrix-chain-multiplication_8230769?challengeSlug=striver-sde-challenge&leftPanelTab=1

---
+++ 170. Matrix Chain Multiplication | Dynamic Programming-1.cpp
@@ -0,0 +1,17 @@
+//CodeStudio Code
+#include <bits/stdc++.h> 
+
+int matrixMultiplication(vector<int> &arr, int N) {
+    vector<vector<int>> dp(N, vector<int>(N, 0));
+    for(int len {2}; len < N; len++) {
+        for(int i {1}; i < N-len+1; i++) {
+          int j{i+len-1};
+          dp[i][j] = INT_MAX;
+          for (int k{i}; k < j; k++) {
+            int cost = dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
+            if(cost < dp[i][j])dp[i][j] = cost;
+            }
+        }
+    }
+    return dp[1][N-1];
+}
